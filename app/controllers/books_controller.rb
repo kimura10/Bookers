a465fc224c68d4763815a6bf0ee44f6cff64abd0
kimura10 <koichi1012kk@gmail.com>
2022-01-08 06:29:42 +0000

    更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,46 @@
 class BooksController < ApplicationController
     
     def index
-        @books = Book.all
-        @book = Book.new
+      @books = Book.all
+      @book = Book.new
+    end
+    
+    def show
+      @book = Book.find(params[:id]) 
+    end
+    
+    def edit
+      @book = Book.find(params[:id])
+   
     end
     
     def create
-        book = Book.new(book_params)
-        book.save
-        redirect_to books_path
+      @books = Book.all
+      @book = Book.new(book_params)
+      if @book.save
+      redirect_to books_path(@book.id)
+        
+      else
+        render 'index'
+      end
+     
+    end
+    
+    def update
+      @books = Book.all
+      @book = Book.find(params[:id])
+       if @book.update(book_params)
+      redirect_to books_path(@book.id)
+      
+       else
+        render 'edit'
+       end
+    end
+    
+    def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
     end
     
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resources :books
+ 
 end
